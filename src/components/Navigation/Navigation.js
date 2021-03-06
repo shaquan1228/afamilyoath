4da1588ece8e079fece23265c822d20a7f6914b1
Shaquan <sdn215@columbia.edu>
2021-03-06 15:05:02 -0500

    pausing work to rebase repo

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from './../Button/Button.js';
 import './assets/Navigation.css';
 
 class Navigation extends React.Component{
@@ -9,11 +10,13 @@ class Navigation extends React.Component{
   }
 
 
+
   render(){
     return (
       <div id="main-nav">
 
-      <div className="main-nav-big" id="main-nav-name"></div>
+      <div className="main-nav-big" id="main-nav-name">
+      </div>
       <div className="main-nav-small" id="main-nav-logo"></div>
       <div className="main-nav-big" id="main-nav-about"></div>
       <div className="main-nav-big" id="main-nav-shop"></div>
