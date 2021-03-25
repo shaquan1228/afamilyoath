be9311408a2ad3a13bf2e09fbb0a354d4c3744df
Carlos Ramirez <carlosmramirez17@gmail.com>
2021-03-25 19:14:40 -0400

    Change url to use state productId variable

--- src/components/ProductSquare/Productsquare.js
+++ src/components/ProductSquare/Productsquare.js
@@ -10,7 +10,7 @@ class ProductSquare extends React.Component{
     this.state = {
       title: "Product Name",
       img: "https://icons.iconarchive.com/icons/iconsmind/outline/256/T-Shirt-icon.png", 
-      productUrl: "oath",
+      productId: "00567",
     }
 
   }
@@ -22,7 +22,7 @@ class ProductSquare extends React.Component{
           <img src={this.state.img} alt="shirt"/>
         </div>
         <div className="description">
-          <h2><Button innerHTML={this.state.title} url="oath" type="main" styleType="light"/></h2>
+          <h2><Button innerHTML={this.state.title} url={this.state.productId} type="main" styleType="light"/></h2>
           <h3>$$$$$$$</h3>
         </div>
       </div>
