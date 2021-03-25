e8203045514537301a3a48368e891fe54131d75c
Carlos Ramirez <carlosmramirez17@gmail.com>
2021-03-24 21:40:18 -0400

    Changing onHover event listener to CSS selector and changing import file path on ProductSquare

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -6,37 +6,12 @@ import './assets/Button.css';
 class Button extends React.Component{
   constructor(props){
     super(props);
-    console.log(props);
     this.state = {
       innerHTML: props.innerHTML,
       url: props.url,
       type: props.type,
       styleType: props.styleType
     };
-
-    this.handleClick = this.handleClick.bind(this);
-    this.handleHover = this.handleHover.bind(this);
-    this.handleHoverExit = this.handleHoverExit.bind(this);
-  }
-
-  componentDidMount() {
-    console.log();
-  }
-
-  handleClick() {
-    //TODO: takes you to the URL generated
-    console.log('CLICK');
-  }
-
-  handleHover(e) {
-    //TODO: tranistion to being underlined
-    e.target.style.transition = "text-decoration .05s ease-in";
-    e.target.style.textDecoration = "underline solid";
-  }
-
-  handleHoverExit(e) {
-    e.target.style.transition = "text-decoration .05s ease-out";
-    e.target.style.textDecoration = "underline transparent";
   }
 
   render(){
@@ -60,8 +35,6 @@ class Button extends React.Component{
         ?       <div
                   className='button'
                   onClick={this.handleClick}
-                  onMouseOver={this.handleHover}
-                  onMouseLeave={this.handleHoverExit}
                   style={myStyle}>
                     <Link to={this.state.url}>{this.state.innerHTML}</Link>
 
@@ -69,8 +42,6 @@ class Button extends React.Component{
         :       <div
                   className='button'
                   onClick={this.handleClick}
-                  onMouseOver={this.handleHover}
-                  onMouseLeave={this.handleHoverExit}
                   style={myStyle}>
                     {this.state.innerHTML}
 
