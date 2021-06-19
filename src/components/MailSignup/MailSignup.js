dc95a190b7afac665c80fc6308d7623e0816eb60
Shaquan <sdn215@columbia.edu>
2021-06-19 11:37:42 -0400

    Fixed small changes. Landing page navigates to shop on click. Mailchimp sign up sans backend also implemented.

---
+++ src/components/MailSignup/MailSignup.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import Button from './../Button/Button.js';
+
+
+
+class MailSignup extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {status:"Subscribe to Newsletter"}
+    
+    this.submitSignup = this.submitSignup.bind(this);
+    this.onType = this.onType.bind(this);
+  }
+
+  onType(e){
+    (e.key === "Enter")
+    ? this.submitSignup()
+    : this.setState({ [e.target.name]: e.target.value })
+
+  }
+  submitSignup(e){
+    let key = "bea80908ace8433e258e543f101f4ca0-us1"
+
+    let url = "http://us1.api.mailchimp.com/2.0/lists/subscribe.json?apikey="+key+"&id=9b99b501b4&email[email]="+
+    this.state.email+"&merge_vars[FNAME]="+this.state.fname+"&merge_vars[LNAME]="+this.state.lname+"&double_optin=true&send_welcome=false"
+    if(!this.state.email){
+      this.setState({status:"Please provide an email address before subscribing."});
+      setTimeout(() => {
+        this.setState({status:"Subscribe to Newsletter", lname: null, fname:null, email:null});
+      },1500)
+    }
+    else if(!this.state.fname){
+      this.setState({status:"Please provide a first name before subscribing."});
+      setTimeout(() => {
+        this.setState({status:"Subscribe to Newsletter", lname: null, fname:null, email:null});
+      },1500)
+    }
+    else if(!this.state.lname){
+      this.setState({status:"Please provide a last name before subscribing."});
+      setTimeout(() => {
+        this.setState({status:"Subscribe to Newsletter", lname: null, fname:null, email:null});
+      },1500)
+    }
+    else if(this.state.fname && this.state.lname && this.state.email) {
+        return fetch(url, {
+                method: 'POST',
+                // mode: 'no-cors',
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Content-Type': 'application/json'
+                },
+                body: null
+            }).then(response => {
+
+                this.setState({status:"Success! Complete Sign Up by Confirming via Email"});
+                setTimeout(() => {
+                  document.getElementById("mail-form-main").style.animation = "clearForm 1.5s ease"
+                  document.getElementsByName("fname")[0].value ="";
+                  document.getElementsByName("lname")[0].value ="";
+                  document.getElementsByName("email")[0].value ="";
+                  this.setState({status:"Subscribe to Newsletter", lname: null, fname:null, email:null});
+                },1500)
+            }).catch(
+                error => this.setState({status:"An error occurred. Please Try Again."})
+            );
+        }
+    }
+  render(){
+    return(
+      <div id="mail-form-main">
+        <input type="text" name="email" placeholder="Email" onKeyDown={this.onType} className="mail-form-email"/>
+        <input type="text" name="fname" placeholder="First Name" onKeyDown={this.onType} className="mail-form-name"/>
+        <input type="text" name="lname" placeholder="Last Name" onKeyDown={this.onType} className="mail-form-name"/>
+        <div className="mail-form-button" onClick={this.submitSignup}>
+            <Button styleType='dark' type={null} innerHTML={this.state.status}/>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+export default MailSignup;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactGA from 'react-ga';
+import MailSignup from './../components/MailSignup/MailSignup.js';
 import './assets/home.css';
 
 
@@ -34,7 +35,6 @@ class Home extends React.Component {
 
         <div id="circle-1" className="circle" style={{ backgroundImage: 'url(' + this.state.imgList[5] + ')' }}>  </div>
 
-
         <div id="circle-6" className="circle" style={{ backgroundImage: 'url(' + this.state.imgList[3] + ')' }}> </div>
         <div id="circle-7" className="circle" style={{ backgroundImage: 'url(' + this.state.imgList[4] + ')' }}> </div>
         <div id="circle-8" className="circle" style={{ backgroundImage: 'url(' + this.state.imgList[1] + ')' }}> </div>
@@ -44,9 +44,11 @@ class Home extends React.Component {
 
         <div id="circle-3" className="circle" style={{ backgroundImage: 'url(' + this.state.imgList[2] + ')' }}> </div>
         <div id="circle-4" className="circle" style={{ backgroundImage: 'url(' + this.state.imgList[0] + ')' }}> </div>
-        <div id="circle-5" className="circle" style={{ backgroundImage: 'url(' + this.state.imgList[0] + ')' }}> </div>
+        <div id="circle-5" className="circle" onClick={()=>{this.props.history.push("./shop")}} style={{ backgroundImage: 'url(' + this.state.imgList[0] + ')' }}> </div>
         <div id="circle-10" className="circle" style={{ backgroundImage: 'url(' + this.state.imgList[1] + ')' }}> </div>
-
+        <div id="mail-form">
+          <MailSignup/>
+        </div>
       </div>
     )
   }
